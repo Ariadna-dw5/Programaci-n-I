5c196c39e8a9d71a65530feafbca11cbe8481c51
Ariadna <ariadna.dw5@gmail.com>
2026-08-16 00:49:09 -0600

    Add files via upload

---
+++ HolamundoS1.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundos1;
+
+/**
+ *
+ * @author Klari
+ */
+public class HolamundoS1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo");
+        System.out.println("Esto ta complicado...");
+        
+        System.out.print ("Soy Arianda");
+        System.out.print ("Quiero aprender a programar");
+    
+        System.out.printf ("Esto es la clase de programacion %S \n ","yei, programar");
+        //System.out.print ("");
+        
+        System.out.printf ("%S Esto es la clase de programacion \n ","yei, programar");
+        //System.out.print ("");
+        
+        System.out.printf ("Esto es la clase %S de programacion \n ","yei, programar");
+        //System.out.print ("");
+        
+        System.out.printf ("Hola \n Como \n estas \n hoy \n amigo");
+        
+        System.out.printf ("Hola \t Como \t estas \t hoy \t amigo");
+        
+        int tiempo;
+        double promedio;
+        String nombre;
+        char inicial;
+        boolean estado;
+        
+        tiempo = 3;
+        nombre = "Ariadna";
+        inicial = 'A';
+        
+        System.out.printf ("Tu nombre es: %s \n", nombre);
+        System.out.printf ("%d veces a la semana recibes la clase \n", tiempo);
+        System.out.printf ("Te llamare %c inicial", inicial);
+        
+        
+        
+        
+        }// Fin de Main
+         
+        // TODO code application logic here
+    } // Fin de la Clase 
